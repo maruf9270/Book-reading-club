52089e5398a7af0029b3c550247f002111f5f570
Maruf9270 <Maruf9270@gmail.com>
2022-09-29 20:54:43 +0600

    Fixed Navber

--- src/Components/Navber/Navber.js
+++ src/Components/Navber/Navber.js
@@ -12,8 +12,8 @@ const Navber = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a href='/'>Item 1</a></li>
-                            <li tabIndex={0}>
+                            <li><a href='/'><button className='text-white btn btn-success'>Log out</button></a></li>
+                            {/* <li tabIndex={0}>
                             <a className="justify-between" href='/'>
                                 Parent
                                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
@@ -23,7 +23,7 @@ const Navber = () => {
                                 <li><a href='/'>Submenu 2</a></li>
                             </ul>
                             </li>
-                            <li><a href='/'>Item 3</a></li>
+                            <li><a href='/'>Item 3</a></li> */}
                         </ul>
                         </div>
                        
@@ -31,25 +31,16 @@ const Navber = () => {
                        
                       
                     </div>
-                    <div className="navbar-center hidden lg:flex">
+                    <div className="navbar-end hidden lg:flex">
                         <ul className="menu menu-horizontal p-0">
-                        <li><a href='/'>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <a href='/'>
-                            Parent
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-                            </a>
-                            <ul className="p-2">
-                            <li><a href='/'>Submenu 1</a></li>
-                            <li><a href='/'>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a href='/'>Item 3</a></li>
+                        
+                        
+                        <li><a href='/'><button className='text-white btn btn-success'>Log out</button></a></li>
                         </ul>
                     </div>
-                    <div className="navbar-end">
+                    {/* <div className="navbar-end">
                         <a className="btn" href='/'>Get started</a>
-                    </div>
+                    </div> */}
                     </div>
 
 
